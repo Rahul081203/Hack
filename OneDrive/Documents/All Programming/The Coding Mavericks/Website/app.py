ff40cdabdd3d730355d60ab3f69e26b52a01cd74
Rahul <rahulthor.081203@gmail.com>
2023-02-15 11:06:44 +0530

    Added some other changes

--- OneDrive/Documents/All Programming/The Coding Mavericks/Website/app.py
+++ OneDrive/Documents/All Programming/The Coding Mavericks/Website/app.py
@@ -3,8 +3,6 @@ import pickle
 import numpy as np
 import predictor as pred
 app=Flask(__name__)
-model=pickle.load(open('model.pkl','rb'))
-
 @app.route('/')
 def index():
     return render_template('home.html')
@@ -15,7 +13,6 @@ def predict():
     symptom_list=symptom.split(',')
     prediction=pred.predictDisease(symptom_list)
     return render_template("prediction.html",data=prediction)
-
 
 if __name__=="__main__":
     app.run()
