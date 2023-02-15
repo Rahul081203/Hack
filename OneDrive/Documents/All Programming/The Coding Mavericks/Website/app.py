358af2e7ce041eada6519b73ea74f3e329c2bd27
Rahul <rahulthor.081203@gmail.com>
2023-02-15 10:23:51 +0530

    Added Backend

---
+++ OneDrive/Documents/All Programming/The Coding Mavericks/Website/app.py
@@ -0,0 +1,21 @@
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
+import predictor as pred
+app=Flask(__name__)
+model=pickle.load(open('model.pkl','rb'))
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/predict',methods=['GET','POST'])
+def predict():
+    symptom=request.form['symptoms']
+    symptom_list=symptom.split(',')
+    prediction=pred.predictDisease(symptom_list)
+    return render_template("prediction.html",data=prediction)
+
+
+if __name__=="__main__":
+    app.run()
